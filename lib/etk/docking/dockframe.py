188c525f57267faa84e2748447a1a130f6d6849b
Arjan Molenaar <gaphor@gmail.com>
2010-08-23 18:30:22 +0200

    Made DockFrame a direct descendant of gtk.Container. Should ease implementation of PlaceHolder window.

--- lib/etk/docking/dockframe.py
+++ lib/etk/docking/dockframe.py
@@ -23,17 +23,92 @@ from __future__ import absolute_import
 from logging import getLogger
 
 import gtk
+from gtk import gdk
 
 
-class DockFrame(gtk.EventBox):
+class DockFrame(gtk.Container):
     """
     Top level widget for a dock layout hierarchy.
     """
-    __gtype_name__ = 'EtkDockLayout'
+    __gtype_name__ = 'EtkDockFrame'
 
     def __init__(self):
-        gtk.EventBox.__init__(self)
+        gtk.Container.__init__(self)
 
         # Initialize logging
         self.log = getLogger('%s.%s' % (self.__gtype_name__, hex(id(self))))
         self.log.debug('')
+        self.child = None
+
+    ############################################################################
+    # GtkWidget
+    ############################################################################
+    def do_realize(self):
+        self.log.debug('')
+
+        # Internal housekeeping
+        self.set_flags(self.flags() | gtk.REALIZED)
+        self.window = gdk.Window(self.get_parent_window(),
+                                 x = self.allocation.x,
+                                 y = self.allocation.y,
+                                 width = self.allocation.width,
+                                 height = self.allocation.height,
+                                 window_type = gdk.WINDOW_CHILD,
+                                 wclass = gdk.INPUT_OUTPUT,
+                                 event_mask = (gdk.EXPOSURE_MASK |
+                                               gdk.POINTER_MOTION_MASK |
+                                               gdk.BUTTON_PRESS_MASK |
+                                               gdk.BUTTON_RELEASE_MASK))
+        self.window.set_user_data(self)
+        #self.style.attach(self.window)
+        #self.style.set_background(self.window, gtk.STATE_NORMAL)
+
+        if self.child:
+            self.child.set_parent_window(self.window)
+
+    def do_unrealize(self):
+        self.log.debug('')
+        self.window.destroy()
+        gtk.Container.do_unrealize(self)
+
+    def do_size_request(self, requisition):
+        self.log.debug('%s' % requisition)
+
+        # Compute total size request
+        width = height = 0
+        if self.child:
+            width, height = self.child.size_request()
+
+        requisition.width = width
+        requisition.height = height
+
+    def do_size_allocate(self, allocation):
+        self.log.debug('%s' % allocation)
+
+        self.allocation = allocation
+        if self.flags() & gtk.REALIZED:
+            self.window.move_resize(*allocation)
+
+        if self.child:
+            bw = self.props.border_width
+            self.child.size_allocate((0, 0,
+                    allocation.width - 2*bw, allocation.height - 2*bw))
+
+    def do_forall(self, internals, callback, data):
+        # Is also called for map and expose events.
+        self.log.debug('%s' % self.child)
+        if self.child:
+            callback(self.child, data)
+
+    def do_add(self, widget):
+        self.log.debug('')
+        assert not self.child
+        widget.set_parent(self)
+        self.child = widget
+
+    def do_remove(self, widget):
+        self.log.debug('')
+        assert self.child
+        self.child.unparent()
+        self.child = None
+
