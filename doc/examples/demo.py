dieterv/etk.docking

commit 15fd00a456d391044320a5704d54e182455ae9a9

DockFrame: move to gtk.Bin, cleanup

lib/etk/docking/dockframe.py
@@ -26,102 +26,61 @@ import gtk
 from gtk import gdk
 
 
-class DockFrame(gtk.Container):
-    """
-    Top level widget for a dock layout hierarchy.
-    """
+class DockFrame(gtk.Bin):
+    '''
+    The etk.DockFrame widget is a gtk.Bin that acts as the toplevel widget
+    for a dock layout hierarchy.
+    '''
     __gtype_name__ = 'EtkDockFrame'
 
     def __init__(self):
-        gtk.Container.__init__(self)
+        gtk.Bin.__init__(self)
 
         # Initialize logging
         self.log = getLogger('%s.%s' % (self.__gtype_name__, hex(id(self))))
         self.log.debug('')
-        self.child = None
-        self.placeholder = None
+
+        # Internal housekeeping
+        self._placeholder = None
 
     ############################################################################
     # GtkWidget
     ############################################################################
-    def do_realize(self):
-        self.log.debug('')
-
-        # Internal housekeeping
-        self.set_flags(self.flags() | gtk.REALIZED)
-        self.window = gdk.Window(self.get_parent_window(),
-                                 x = self.allocation.x,
-                                 y = self.allocation.y,
-                                 width = self.allocation.width,
-                                 height = self.allocation.height,
-                                 window_type = gdk.WINDOW_CHILD,
-                                 wclass = gdk.INPUT_OUTPUT,
-                                 event_mask = (gdk.EXPOSURE_MASK |
-                                               gdk.POINTER_MOTION_MASK |
-                                               gdk.BUTTON_PRESS_MASK |
-                                               gdk.BUTTON_RELEASE_MASK))
-        self.window.set_user_data(self)
-        self.style.attach(self.window)
-        self.style.set_background(self.window, gtk.STATE_NORMAL)
-
-        if self.child:
-            self.child.set_parent_window(self.window)
-
-    def do_unrealize(self):
-        self.log.debug('')
-        self.window.destroy()
-        gtk.Container.do_unrealize(self)
-
     def do_size_request(self, requisition):
         self.log.debug('%s' % requisition)
 
-        # Compute total size request
-        width = height = 0
-        if self.child:
-            width, height = self.child.size_request()
+        requisition.width = 0
+        requisition.height = 0
 
-        requisition.width = width
-        requisition.height = height
+        if self.child and self.child.flags() & gtk.VISIBLE:
+            (requisition.width, requisition.height) = self.child.size_request()
+            requisition.width += self.border_width * 2
+            requisition.height += self.border_width * 2
 
     def do_size_allocate(self, allocation):
         self.log.debug('%s' % allocation)
 
         self.allocation = allocation
-        if self.flags() & gtk.REALIZED:
-            self.window.move_resize(*allocation)
 
-        if self.child:
-            bw = self.props.border_width
-            self.child.size_allocate((0, 0,
-                    allocation.width - 2*bw, allocation.height - 2*bw))
-        #if self.placeholder:
-        #    self.placeholder.size_allocate((0, 0, 200, 200))
+        if self.child and self.child.flags() & gtk.VISIBLE:
+            child_allocation = gdk.Rectangle()
+            child_allocation.x = allocation.x + self.border_width
+            child_allocation.y = allocation.y + self.border_width
+            child_allocation.width = allocation.width - (2 * self.border_width)
+            child_allocation.height = allocation.height - (2 * self.border_width)
+            self.child.size_allocate(child_allocation)
 
     def do_forall(self, internals, callback, data):
         # Is also called for map and expose events.
         if self.child:
             callback(self.child, data)
 
-        if internals and self.placeholder:
-            callback(self.placeholder, data)
-
-    def do_add(self, widget):
-        self.log.debug('')
-        assert not self.child
-        widget.set_parent(self)
-        self.child = widget
-
-    def do_remove(self, widget):
-        self.log.debug('')
-        assert self.child
-        self.child.unparent()
-        self.child = None
-
     def set_placeholder(self, placeholder):
-        if self.placeholder:
-            self.placeholder.unparent()
-            self.placeholder = None
-        if placeholder:
-            self.placeholder = placeholder
-            self.placeholder.set_parent(self)
+        if self._placeholder:
+            self._placeholder.hide()
+            self._placeholder.destroy()
+            self._placeholder = None
 
+        if placeholder:
+            self._placeholder = placeholder
+            self._placeholder.set_transient_for(self.get_toplevel())

commit 883123ff9313d0c175b7c274095e50cc3b35474e

Fixed placeholder placement, broken in 15fd00a4.

doc/examples/demo.py
@@ -66,6 +66,7 @@ class MainWindow(gtk.Window):
             self.docklayout = docklayout
         else:
             self.dockframe = DockFrame()
+            self.dockframe.props.border_width = 8
             self.dockframe.add(DockGroup())
             self.docklayout = DockLayout()
             self.docklayout.add(self.dockframe)

lib/etk/docking/dockframe.py
@@ -74,13 +74,21 @@ class DockFrame(gtk.Bin):
         # Is also called for map and expose events.
         if self.child:
             callback(self.child, data)
+#        if internals and self._placeholder:
+#            callback(self._placeholder, data)
 
     def set_placeholder(self, placeholder):
+        """
+        Set a new placeholder widget on the frame. The placeholder is drawn on top
+        of the dock items.
+
+        If a new placeholder is set, an existing placeholder is destroyed.
+        """
         if self._placeholder:
-            self._placeholder.hide()
+            self._placeholder.unparent()
             self._placeholder.destroy()
             self._placeholder = None
 
         if placeholder:
             self._placeholder = placeholder
-            self._placeholder.set_transient_for(self.get_toplevel())
+            self._placeholder.set_parent(self)

lib/etk/docking/docklayout.py
@@ -240,27 +240,39 @@ def make_placeholder(widget, x, y, a, ca=None):
     if ca is None:
         ca = a
 
+    if isinstance(widget, DockFrame):
+        print 'widget is frame', widget.allocation
+        frame = widget
+        bw = frame.border_width
+    else:
+        bw = 0
+
     if x < MAGIC_BORDER_SIZE:
-        allocation = (ca.x, ca.y, MAGIC_BORDER_SIZE, ca.height)
+        allocation = (a.x, a.y, MAGIC_BORDER_SIZE, ca.height - 2*bw)
     elif a.width - x < MAGIC_BORDER_SIZE:
-        allocation = (a.width - MAGIC_BORDER_SIZE, ca.y, MAGIC_BORDER_SIZE, ca.height)
+        allocation = (a.width - MAGIC_BORDER_SIZE - 2*bw, ca.y, MAGIC_BORDER_SIZE, ca.height - 2*bw)
     elif y < MAGIC_BORDER_SIZE:
-        allocation = (ca.x, ca.y, ca.width, MAGIC_BORDER_SIZE)
+        allocation = (ca.x, ca.y, ca.width - 2*bw, MAGIC_BORDER_SIZE)
     elif a.height - y < MAGIC_BORDER_SIZE:
-        allocation = (ca.x, a.height - MAGIC_BORDER_SIZE, ca.width, MAGIC_BORDER_SIZE)
+        allocation = (ca.x, a.height - MAGIC_BORDER_SIZE - 2*bw, ca.width - 2*bw, MAGIC_BORDER_SIZE)
 
     placeholder = Placeholder()
 
-    if isinstance(widget, DockFrame):
-        frame = widget
-    else:
+    if not isinstance(widget, DockFrame):
         frame = widget.get_ancestor(DockFrame)
         fx, fy = widget.translate_coordinates(frame, allocation[0], allocation[1])
         allocation = (fx, fy, allocation[2], allocation[3])
 
+    a = frame.allocation
+    allocation = (a[0] + allocation[0] + bw,
+                  a[1] + allocation[1] + bw,
+                  allocation[2],
+                  allocation[3])
+
     frame.set_placeholder(placeholder)
     placeholder.size_allocate(allocation)
     placeholder.show()
+    #placeholder.get_window().move(allocation[0], allocation[1])
 
 def with_magic_borders(func):
     '''