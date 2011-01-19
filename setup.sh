aae95c9ae3b36028c6474124db27f4a8350dc642
Dieter Verfaillie <dieterv@optionexplicit.be>
2011-01-19 13:55:16 +0100

    Also build source distribution

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ INTERPRETERS="/d/bin/Python27 /d/bin/Python26"
 # Build documentation
 /d/bin/Python27/python setup.py build_sphinx
 
+# Build source distribution
+# There's a bug in gztar on windows: a "dist" dir is created in the .gz
+# part that contains the .tar part. bztar does not have this bug. *sigh*
+/d/bin/Python27/python setup.py sdist --formats=bztar
+
 # Build eggs
 for INTERPRETER in ${INTERPRETERS}; do
     ${INTERPRETER}/python setup.py bdist_egg
