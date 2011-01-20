1cc4af043571029a8cd00a716fa76d86896ce5b5
Dieter Verfaillie <dieterv@optionexplicit.be>
2011-01-20 07:54:19 +0100

    drop the pkgutil fallback as we depend on setuptools anyway

--- lib/etk/__init__.py
+++ lib/etk/__init__.py
@@ -1,9 +1,1 @@
-# -*- coding: utf-8 -*-
-
-# This is a namespace package
-try:
-    import pkg_resources
-    pkg_resources.declare_namespace(__name__)
-except ImportError:
-    import pkgutil
-    __path__ = pkgutil.extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
